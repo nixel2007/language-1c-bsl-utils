b3acf6d6e2b9cd96f09a5b49e466f936feb41683
Nikita Gryzlov <nixel2007@gmail.com>
2016-05-07 16:15:50 +0300

    Обновление пакета

--- src/global.ts
+++ src/global.ts
@@ -1,21 +1,16 @@
-import * as fs from "fs";
 import * as path from "path";
 
 import * as bslglobals from "./bslGlobals";
 
-let exec = require("child-process-promise").exec;
-let iconv = require("iconv-lite");
 let loki = require("lokijs");
 let Parser = require("onec-syntaxparser");
 let FileQueue = require("filequeue");
 let fq = new FileQueue(500);
 
 export class Global {
-    exec: string;
     cache: any;
     db: any;
-    dblocal: any;
-    dbcalls: any;
+    dbcalls: Map<string, Array<{}>>;
     globalfunctions: any;
     globalvariables: any;
     keywords: any;
@@ -24,9 +19,9 @@ export class Global {
     private cacheUpdates: boolean;
 
     getCacheLocal(filename: string, word: string, source, update: boolean = false, allToEnd: boolean = true, fromFirst: boolean = true) {
-        let suffix = allToEnd  ? "" : "$";
+        let suffix = allToEnd ? "" : "$";
         let prefix = fromFirst ? "^" : "";
-        let querystring = {"name": {"$regex": new RegExp(prefix + word + suffix, "i")}};
+        let querystring = { "name": { "$regex": new RegExp(prefix + word + suffix, "i") } };
         let entries = new Parser().parse(source).getMethodsTable().find(querystring);
         return entries;
     }
@@ -44,7 +39,7 @@ export class Global {
         return collection;
     }
 
-    getReplaceMetadata () {
+    getReplaceMetadata() {
         return {
             "AccountingRegisters": "РегистрыБухгалтерии",
             "AccumulationRegisters": "РегистрыНакопления",
@@ -70,60 +65,57 @@ export class Global {
     getModuleForPath(fullpath: string, rootPath: string): any {
         let splitsymbol = "/";
         let moduleArray: Array<string> = fullpath.substr(rootPath.length + (rootPath.slice(-1) === "\\" ? 0 : 1)).split(splitsymbol);
-        let moduleStr: string = "";
-            let hierarchy = moduleArray.length;
-            if (hierarchy > 3) {
-                if (moduleArray[hierarchy - 4].startsWith("CommonModules")) {
-                    moduleStr = moduleArray[hierarchy - 3];
-                } else if (hierarchy > 3 && this.toreplaced[moduleArray[hierarchy - 4]] !== undefined) {
-                    moduleStr = this.toreplaced[moduleArray[hierarchy - 4]] + "." + moduleArray[hierarchy - 3];
-                }
+        let moduleStr = "";
+        let hierarchy = moduleArray.length;
+        if (hierarchy > 3) {
+            if (moduleArray[hierarchy - 4].startsWith("CommonModules")) {
+                moduleStr = moduleArray[hierarchy - 3];
+            } else if (hierarchy > 3 && this.toreplaced[moduleArray[hierarchy - 4]] !== undefined) {
+                moduleStr = this.toreplaced[moduleArray[hierarchy - 4]] + "." + moduleArray[hierarchy - 3];
             }
-
+        }
         return moduleStr;
     }
 
-    addtocachefiles(files: Array<string>, rootPath: any = null): any {
-        if (!rootPath) {
-            rootPath = this.getRootPath();
-        }
+    addtocachefiles(files: Array<string>, rootPath: string): any {
         let filesLength = files.length;
+        let substrIndex = (process.platform === "win32") ? 8 : 7;
         for (let i = 0; i < filesLength; ++i) {
             let fullpath = files[i].toString();
             fullpath = decodeURIComponent(fullpath);
             if (fullpath.startsWith("file:")) {
-                if (process.platform === "win32") {
-                    fullpath = fullpath.substr(8);
-                } else {
-                    fullpath = fullpath.substr(7);
-                }
+                fullpath = fullpath.substr(substrIndex);
             }
-            fq.readFile(fullpath, {encoding: "utf8"}, (err, source) => {
+            fq.readFile(fullpath, { encoding: "utf8" }, (err, source) => {
                 if (err) {
                     throw err;
                 }
-                let moduleStr = "";
-                if (fullpath.endsWith(".bsl")) {
-                    moduleStr = this.getModuleForPath(fullpath, rootPath);
-                }
+                let moduleStr = fullpath.endsWith(".bsl") ? this.getModuleForPath(fullpath, rootPath) : "";
                 source = source.replace(/\r(?!\n)/g, "\r\n")
                 let parsesModule = new Parser().parse(source);
+                source = null;
                 let entries = parsesModule.getMethodsTable().find();
                 if (i % 100 === 0) {
                     this.postMessage("Обновляем кэш файла № " + i + " из " + filesLength, 2000);
                 }
-                this.updateReferenceCalls(this.dbcalls, parsesModule.context.CallsPosition, "GlobalModuleText", fullpath);
+                if (parsesModule.context.CallsPosition.length > 0) {
+                    this.updateReferenceCalls(this.dbcalls, parsesModule.context.CallsPosition, "GlobalModuleText", fullpath);
+                }
+                parsesModule = null;
                 for (let y = 0; y < entries.length; ++y) {
                     let item = entries[y];
-                    this.updateReferenceCalls(this.dbcalls, item._method.CallsPosition, item, fullpath);
-                    item["filename"] = fullpath;
+                    let method = { name: item.name, endline: item.endline, context: item.context, isproc: item.isproc }
+                    if (item._method.CallsPosition.length > 0) {
+                        this.updateReferenceCalls(this.dbcalls, item._method.CallsPosition, method, fullpath);
+                    }
+                    let _method = { Params: item._method.Params, IsExport: item._method.IsExport };
                     let newItem: MethodValue = {
                         "name": String(item.name),
                         "isproc": Boolean(item.isproc),
                         "line": item.line,
                         "endline": item.endline,
                         "context": item.context,
-                        "_method": item._method,
+                        "_method": _method,
                         "filename": fullpath,
                         "module": moduleStr,
                         "description": item.description
@@ -147,10 +139,9 @@ export class Global {
         if (rootPath) {
             rootPath = path.join(rootPath, basePath);
             this.db = this.cache.addCollection("ValueTable");
-            this.dbcalls = this.cache.addCollection("Calls");
-
             let searchPattern = basePath !== "" ? basePath.substr(2) + "/**" : "**/*.{bsl,os}";
             this.findFilesForCache(searchPattern, rootPath);
+            this.dbcalls = new Map();
         }
     };
 
@@ -170,18 +161,16 @@ export class Global {
         let parsesModule = new Parser().parse(source);
         let moduleStr = this.getModuleForPath(fullpath, rootPath);
         let entries = parsesModule.getMethodsTable().find();
-        this.updateReferenceCalls(this.dbcalls, parsesModule.context.CallsPosition, "GlobalModuleText", filename);
         for (let y = 0; y < entries.length; ++y) {
             let item = entries[y];
-            this.updateReferenceCalls(this.dbcalls, item._method.CallsPosition, item, filename);
-            item["filename"] = filename;
+            let _method = { Params: item._method.Params, IsExport: item._method.IsExport };
             let newItem = {
                 "name": String(item.name),
                 "isproc": Boolean(item.isproc),
                 "line": item.line,
                 "endline": item.endline,
                 "context": item.context,
-                "_method": item._method,
+                "_method": _method,
                 "filename": fullpath,
                 "module": moduleStr,
                 "description": item.description
@@ -190,38 +179,28 @@ export class Global {
         }
     };
 
-
-    queryref(word: string, collection: any, local: boolean = false ): any {
+    queryref(word: string, collection: any, local: boolean = false): any {
         if (!collection) {
             return new Array();
         }
         let prefix = local ? "" : ".";
-        let querystring = {"call": {"$regex": new RegExp(prefix + word + "$", "i")}};
+        let querystring = { "call": { "$regex": new RegExp(prefix + word + "$", "i") } };
         let search = collection.chain().find(querystring).simplesort("name").data();
         return search;
     }
 
     private updateReferenceCalls(collection: any, calls: Array<any>, method: any, file: string): any {
-        if (!collection) {
-            collection = this.cache.addCollection("Calls");
-        }
-        let self = this;
         for (let index = 0; index < calls.length; index++) {
             let value = calls[index];
-            if (value.call.startsWith(".")) {
+            if (value.call.indexOf(".") === -1) {
                 continue;
             }
-            let newItem: MethodValue = {
-                "name": String(method.name),
-                "filename": file,
-                "isproc": Boolean(method.isproc),
-                "call": value.call,
-                "context": method.context,
-                "line": value.line,
-                "character": value.character,
-                "endline": method.endline
-            };
-            collection.insert(newItem);
+            let arrCalls = this.dbcalls.get(value.call);
+            if (!arrCalls) {
+                this.dbcalls.set(value.call,[])
+                arrCalls = this.dbcalls.get(value.call);
+            }
+            arrCalls.push({ filename: file, call: value.call, line: value.line, character: value.character, name: String(method.name) });
         }
     }
 
@@ -232,37 +211,33 @@ export class Global {
             return new Array();
         } else {
             let prefix = lazy ? "" : "^";
-            let suffix = all  ? "" : "$";
-            let querystring = {"module": {"$regex": new RegExp(prefix + module + suffix, "i")}};
+            let suffix = all ? "" : "$";
+            let querystring = { "module": { "$regex": new RegExp(prefix + module + suffix, "i") } };
             let search = this.db.chain().find(querystring).simplesort("name").data();
             return search;
         }
     }
 
     query(word: string, module: string, all: boolean = true, lazy: boolean = false): any {
-        if (!this.cacheUpdates) {
-            return new Array();
-        } else {
-            let prefix = lazy ? "" : "^";
-            let suffix = all  ? "" : "$";
-            let querystring = {"name": {"$regex": new RegExp(prefix + word + suffix, "i")}};
-            if (module && module.length > 0) {
-                querystring["module"] = {"$regex": new RegExp("^" + module + "", "i")};
-            }
-            let moduleRegexp = new RegExp("^" + module + "$", "i");
-            function filterByModule(obj) {
-                if (module && module.length > 0) {
-                    if (moduleRegexp.exec(obj.module) != null) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            let search = this.db.chain().find(querystring).where(filterByModule).simplesort("name").data();
-            return search;
+        let prefix = lazy ? "" : "^";
+        let suffix = all ? "" : "$";
+        let querystring = { "name": { "$regex": new RegExp(prefix + word + suffix, "i") } };
+        if (module && module.length > 0) {
+            querystring["module"] = { "$regex": new RegExp("^" + module + "", "i") };
         }
+        let moduleRegexp = new RegExp("^" + module + "$", "i");
+        function filterByModule(obj) {
+            if (module && module.length > 0) {
+                if (moduleRegexp.exec(obj.module) != null) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+        let search = this.db.chain().find(querystring).where(filterByModule).simplesort("name").data();
+        return search;
     }
 
     GetSignature(entry) {
@@ -288,8 +263,7 @@ export class Global {
         if (strRetState) {
             paramsString = paramsString + ": " + strRetState;
         }
-
-        return { description: description, paramsString: paramsString, strRetState: strRetState, fullRetState: (!retState) ? "" : retState[0]};
+        return { description: description, paramsString: paramsString, strRetState: strRetState, fullRetState: (!retState) ? "" : retState[0] };
     }
 
     GetDocParam(description: string, param) {
@@ -302,7 +276,6 @@ export class Global {
             let cast = (new RegExp("\\n\\s*[<\\wа-яА-Я\\.>]+\\s*(-|–)\\s*", "g")).exec(descriptionParam);
             if (cast) {
                 descriptionParam = descriptionParam.substr(0, cast.index);
-
             }
         }
         let documentationParam = { optional: optional, descriptionParam: descriptionParam };
@@ -328,10 +301,7 @@ export class Global {
     constructor(exec: string) {
         let configuration = this.getConfiguration("language-1c-bsl");
         let autocompleteLanguage: any = this.getConfigurationKey(configuration, "languageAutocomplete");
-        let postfix = "";
-        if (autocompleteLanguage === "en") {
-            postfix = "_en";
-        }
+        let postfix = (autocompleteLanguage === "en") ? "_en" : "";
         this.toreplaced = this.getReplaceMetadata();
         this.cache = new loki("gtags.json");
         this.cacheUpdates = false;
@@ -343,7 +313,7 @@ export class Global {
         for (let element in globalfunctions) {
             let new_name = globalfunctions[element]["name" + postfix];
             let new_element = {};
-            new_element["name"] =  new_name;
+            new_element["name"] = new_name;
             new_element["description"] = globalfunctions[element].description;
             new_element["signature"] = globalfunctions[element].signature;
             this.globalfunctions[new_name.toLowerCase()] = new_element;
@@ -351,7 +321,7 @@ export class Global {
         for (let element in globalvariables) {
             let new_name = globalvariables[element]["name" + postfix];
             let new_element = {};
-            new_element["name"] =  new_name;
+            new_element["name"] = new_name;
             new_element["description"] = globalvariables[element].description;
             this.globalvariables[new_name.toLowerCase()] = new_element;
         }
